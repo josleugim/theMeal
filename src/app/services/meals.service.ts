199b3b3cf1aeba82f349a7d1bb1db1892f7add8b
Jose Miguel <josleugim@gmail.com>
2022-03-08 21:31:08 -0600

    Changed API URL for the exercise

--- src/app/services/meals.service.ts
+++ src/app/services/meals.service.ts
@@ -10,14 +10,14 @@ export class MealsService {
   constructor(private http: HttpClient) { }
 
   findAll(): Observable<any> {
-    return this.http.get(`/meals`);
+    return this.http.get(`/api/json/v1/1/search.php?s=`);
   }
 
   findRandom(): Observable<any> {
-    return this.http.get(`/meals-random`);
+    return this.http.get(`/api/json/v1/1/random.php`);
   }
 
   findById(id: number = 52977): Observable<any> {
-    return this.http.get(`/meals/${id}`);
+    return this.http.get(`/api/json/v1/1/lookup.php?i=${id}`);
   }
 }
