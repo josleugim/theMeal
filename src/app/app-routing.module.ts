c4cee6ebf6888e4a97f0e5f1b3036f6a6474ba01
Jose Miguel <josleugim@gmail.com>
2022-03-08 21:27:09 -0600

    Added simple RouteGuard and login markup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {IngredientComponent} from "./ingredient/ingredient.component";
+import {AuthGuardGuard} from "./guards/auth-guard.guard";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {
-    path: '',component: HomeComponent
+    path: '', component: HomeComponent
   },
   {
     path: 'dishes', loadChildren: () => import('./dishes/dishes.module').then(m => m.DishesModule)
+  },
+  {
+    path: 'ingredients', component: IngredientComponent, canActivate: [AuthGuardGuard]
+  },
+  {
+    path: 'login', component: LoginComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from "./shared/shared.module";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatButtonModule} from "@angular/material/button";
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { LoginComponent } from './login/login.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    IngredientComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +32,8 @@ import {MatButtonModule} from "@angular/material/button";
     SharedModule,
     MatMenuModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
